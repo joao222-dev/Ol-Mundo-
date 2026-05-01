bb10ba90657a5e4b94615d73d6012d99ad487e4c
joao222-dev <jpfs222@gmail.com>
2026-05-01 10:43:46 -0300

    readme alterado

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Olá Mundo!
 Primeiro repositório versionado
 
+Buscando evoluir meus conhecimentos em python e aprender cada vez mais sobre.
---
+++ python_estudos.py
@@ -0,0 +1,56 @@
+# def cadastrar_peca(pecas):
+#     nome = input("Digite a peça: ")
+#     pecas.append(nome)
+
+# def listar_pecas(pecas):
+#     for peca in pecas:
+#         print(peca)
+
+# def buscar_peca(pecas):
+#     nome = input("Digite a peça para buscar: ")
+    
+#     if nome in pecas:
+#         print("Peça encontrada")
+#     else:
+#         print("Peça não encontrada")
+
+# def menu():
+#     pecas = []
+
+#     while True:
+#         print("\n1 - Cadastrar")
+#         print("2 - Listar")
+#         print("3 - Buscar")
+#         print("4 - Sair")
+
+#         opcao = input("Escolha: ")
+
+#         if opcao == "1":
+#             cadastrar_peca(pecas)
+#         elif opcao == "2":
+#             listar_pecas(pecas)
+#         elif opcao == "3":
+#             buscar_peca(pecas)
+#         elif opcao == "4":
+#             break
+#         else:
+#             print("Opção inválida")
+
+
+nome = "joao paulo"
+email = "emailfalsodolira@gmail.com"
+#descobrir o servidor 1!nome usuario e msg usuario x foi cadastrado no email x
+# io = nome.find(" ") 
+# io = nome[:io].capitalize()
+# print (io)
+nome_usuario = nome.split()[0].capitalize()
+print (f"Nome: {nome_usuario}")
+
+servidor = email.find("@")
+servidor = email[servidor+1:]
+print (f"Servidor:{servidor}")
+usuario = email.find("@")
+usuario = email[:usuario]
+print (f"Usuario:{usuario}")
+print (f"Usuario {nome_usuario} foi cadastrado no email {nome_usuario}@{servidor}")
+
